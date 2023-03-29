e447deeb84a5e2348a573dd6eeb87b41ede385dd
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2023-03-28 23:49:42 -0300

    second day practice
    used next, image, head and link components, also created images folder and components folder, as well as integrated some basic css

--- src/pages/breakfast/firstbreakfast.js
+++ src/pages/breakfast/firstbreakfast.js
@@ -1,14 +1,21 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function firstBreakfast() {
 	return (
-		<> 
-			<h1>
-				Breakfast Number One
-			</h1>
-			<h2>
-				<Link href="/">Home</Link>
-			</h2>
+		<>
+			<Layout>
+				<Head>
+					<title>DietApp - Breakfast Options</title>
+				</Head>
+				<h1>
+					Breakfast Number One
+				</h1>
+				<h2>
+					<Link href="/">Home</Link>
+				</h2>
+			</Layout>
 		</>
 	);
 }
